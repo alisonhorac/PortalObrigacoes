d4cb255218b6225c5f2781fe75407ec694972fb7
Alison Horacio <alisonhorac@hotmail.com>
2019-05-18 21:06:53 -0300

    Ajustes do layout da tela inicial

--- src/AHAS.PO.UI.SITE/App_Start/BundleConfig.cs
+++ src/AHAS.PO.UI.SITE/App_Start/BundleConfig.cs
@@ -8,23 +8,77 @@ namespace AHAS.PO.UI.SITE
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            RegisterLayout(bundles);
+            RegisterShared(bundles);
+            RegisterAccount(bundles);
+        }
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+        private static void RegisterAccount(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/Scripts/Account/Login").Include(
+                "~/Scripts/Account/Login.js"));
 
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+            bundles.Add(new ScriptBundle("~/Scripts/Account/Register").Include(
+                "~/Scripts/Account/Register.js"));
+        }
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+        private static void RegisterShared(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/Scripts/Shared/_Layout").Include(
+                "~/Scripts/Shared/_Layout.js"));
+        }
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+        private static void RegisterLayout(BundleCollection bundles)
+        {
+            // bootstrap
+            bundles.Add(new ScriptBundle("~/Content/bootstrap/js").Include(
+                "~/Content/bootstrap/js/bootstrap.min.js"));
+
+            bundles.Add(new StyleBundle("~/Content/bootstrap/css").Include(
+                "~/Content/bootstrap/css/bootstrap.min.css"));
+
+            // dist
+            bundles.Add(new ScriptBundle("~/Content/dist/js").Include(
+                "~/Content/dist/js/app.js"));
+
+            bundles.Add(new StyleBundle("~/Content/dist/css").Include(
+                "~/Content/dist/css/admin-lte.min.css"));
+
+            bundles.Add(new StyleBundle("~/Content/dist/css/skins").Include(
+                "~/Content/dist/css/skins/_all-skins.min.css"));
+
+            // plugins | jquery
+            bundles.Add(new ScriptBundle("~/Content/plugins/jquery/js").Include(
+                                         "~/Content/plugins/jquery/js/jQuery-2.1.4.min.js"));
+
+            // plugins | font-awesome
+            bundles.Add(new StyleBundle("~/Content/plugins/font-awesome/css").Include(
+                                        "~/Content/plugins/font-awesome/css/font-awesome.min.css"));
+
+            // plugins | ionicons
+            bundles.Add(new StyleBundle("~/Content/plugins/ionicons/css").Include(
+                                        "~/Content/plugins/ionicons/css/ionicons.min.css"));
+
+            // plugins | fastclick
+            bundles.Add(new ScriptBundle("~/Content/plugins/fastclick/js").Include(
+                                         "~/Content/plugins/fastclick/js/fastclick.min.js"));
+
+            // plugins | slimscroll
+            bundles.Add(new ScriptBundle("~/Content/plugins/slimscroll/js").Include(
+                                         "~/Content/plugins/slimscroll/js/jquery.slimscroll.min.js"));
+
+            // plugins | checkbox
+            bundles.Add(new ScriptBundle("~/Content/plugins/icheck/js").Include(
+                                         "~/Content/plugins/icheck/js/icheck.min.js"));
+
+            bundles.Add(new StyleBundle("~/Content/plugins/icheck/css").Include(
+                                        "~/Content/plugins/icheck/css/all.css"));
+
+            bundles.Add(new StyleBundle("~/Content/plugins/icheck/css/flat").Include(
+                                        "~/Content/plugins/icheck/css/flat/flat.css"));
+
+            bundles.Add(new StyleBundle("~/Content/plugins/icheck/css/sqare/blue").Include(
+                                        "~/Content/plugins/icheck/css/sqare/blue.css"));
         }
     }
 }
