9620f2bd26a9cccd5c54d17a79531254f2247d9c
Alison Horacio <alisonhorac@hotmail.com>
2019-05-21 23:35:06 -0300

    limpeza na UI, remoção de packages não utilizados e utilizado CDN nas paginas

---
+++ src/AHAS.PO.UI.SITE/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AHAS.PO.UI.SITE.MvcApplication" Language="C#" %>
---
+++ src/AHAS.PO.UI.SITE/Global.asax.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace AHAS.PO.UI.SITE
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+    }
+}
